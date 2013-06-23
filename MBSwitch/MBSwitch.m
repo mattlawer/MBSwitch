5fc983531f358b6d6f4926eccbda434804cdf0b1
Mathieu Bolard <me@mathieubolard.com>
2013-06-24 00:18:49 +0200

    fix Animation

--- MBSwitch/MBSwitch.m
+++ MBSwitch/MBSwitch.m
@@ -101,146 +101,7 @@
 }
 
 #pragma mark -
-#pragma mark Interaction
-
-- (void)tapped:(UITapGestureRecognizer *)gesture
-{	
-	if (gesture.state == UIGestureRecognizerStateEnded)
-		[self setOn:!self.on animated:YES];
-}
-
-- (void)toggleDragged:(UIPanGestureRecognizer *)gesture
-{
-	CGFloat minToggleX = 1.0;
-	CGFloat maxToggleX = self.bounds.size.width-self.bounds.size.height+1.0;
-    
-	if (gesture.state == UIGestureRecognizerStateBegan)
-	{
-		self.pressed = YES;
-        _dragging = YES;
-	}
-	else if (gesture.state == UIGestureRecognizerStateChanged)
-	{
-		CGPoint translation = [gesture translationInView:self];
-        
-		[CATransaction setDisableActions:YES];
-        
-		self.pressed = YES;
-        
-		CGFloat newX = _thumbLayer.frame.origin.x + translation.x;
-		if (newX < minToggleX) newX = minToggleX;
-		if (newX > maxToggleX) newX = maxToggleX;
-		_thumbLayer.frame = CGRectMake(newX,
-                                            _thumbLayer.frame.origin.y,
-                                            _thumbLayer.frame.size.width,
-                                            _thumbLayer.frame.size.height);
-        
-        if (CGRectGetMidX(_thumbLayer.frame) > CGRectGetMidX(self.bounds)
-            && ![[_backLayer valueForKey:@"isOn"] boolValue]) {
-            [self setBackgroundOn:YES animated:YES];
-        }else if (CGRectGetMidX(_thumbLayer.frame) < CGRectGetMidX(self.bounds)
-                  && [[_backLayer valueForKey:@"isOn"] boolValue]){
-            [self setBackgroundOn:NO animated:YES];
-        }
-        
-        
-		[gesture setTranslation:CGPointZero inView:self];
-	}
-	else if (gesture.state == UIGestureRecognizerStateEnded)
-	{
-		CGFloat toggleCenter = CGRectGetMidX(_thumbLayer.frame);
-        [self setOn:(toggleCenter > CGRectGetMidX(self.bounds)) animated:YES];
-        _dragging = NO;
-        self.pressed = NO;
-	}
-    
-	CGPoint locationOfTouch = [gesture locationInView:self];
-	if (CGRectContainsPoint(self.bounds, locationOfTouch))
-		[self sendActionsForControlEvents:UIControlEventTouchDragInside];
-	else
-		[self sendActionsForControlEvents:UIControlEventTouchDragOutside];
-}
-
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
-{    
-	[super touchesBegan:touches withEvent:event];
-    
-    self.pressed = YES;
-	
-	[self sendActionsForControlEvents:UIControlEventTouchDown];
-}
-
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
-{
-	[super touchesEnded:touches withEvent:event];
-    if (!_dragging) {
-        self.pressed = NO;
-    }
-	[self sendActionsForControlEvents:UIControlEventTouchUpInside];
-}
-
-- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
-{
-	[super touchesCancelled:touches withEvent:event];
-    if (!_dragging) {
-        self.pressed = NO;
-    }
-	[self sendActionsForControlEvents:UIControlEventTouchUpOutside];
-}
-
-- (void) setPressed:(BOOL)pressed {
-    if (_pressed != pressed) {
-        _pressed = pressed;
-        
-        if (!_on) {
-            [self showFillLayer:!_pressed animated:YES];
-        }
-    }
-}
-
-- (void) setBackgroundOn:(BOOL)on animated:(BOOL)animated {
-    BOOL isOn = [[_backLayer valueForKey:@"isOn"] boolValue];
-    if (on != isOn) {
-        if (animated) {
-            CABasicAnimation *animateColor = [CABasicAnimation animationWithKeyPath:@"fillColor"];
-            animateColor.duration = 0.22;
-            animateColor.fromValue = on ? (id)_tintColor.CGColor : (id)_onTintColor.CGColor;
-            animateColor.toValue = on ? (id)_onTintColor.CGColor : (id)_tintColor.CGColor;
-            animateColor.removedOnCompletion = NO;
-            animateColor.fillMode = kCAFillModeForwards;
-            [_backLayer setValue:[NSNumber numberWithBool:on] forKey:@"isOn"];
-            [_backLayer addAnimation:animateColor forKey:@"animateColor"];
-            [CATransaction commit];
-        }else {
-            [_backLayer removeAllAnimations];
-            [_backLayer setValue:[NSNumber numberWithBool:on] forKey:@"isOn"];
-            _backLayer.fillColor = on ? _onTintColor.CGColor : _tintColor.CGColor;
-        }
-    }
-}
-
-- (void) showFillLayer:(BOOL)show animated:(BOOL)animated {
-    BOOL isVisible = [[_fillLayer valueForKey:@"isVisible"] boolValue];
-    if (isVisible != show) {
-        [_fillLayer setValue:[NSNumber numberWithBool:show] forKey:@"isVisible"];
-        CGFloat scale = show ? 1.0 : 0.0;
-        if (animated) {
-            CGFloat from = show ? 0.0 : 1.0;
-            CABasicAnimation *animateScale = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
-            animateScale.duration = 0.22;
-            animateScale.fromValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(from, from, 1.0)];
-            animateScale.toValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(scale, scale, 1.0)];
-            animateScale.removedOnCompletion = NO;
-            animateScale.fillMode = kCAFillModeForwards;
-            animateScale.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-            [_fillLayer addAnimation:animateScale forKey:@"animateScale"];
-        }else {
-            [_fillLayer removeAllAnimations];
-            _fillLayer.transform = CATransform3DMakeScale(scale,scale,1.0);
-        }
-    }
-}
-
+#pragma mark Animations
 
 - (BOOL) isOn {
     return _on;
@@ -269,6 +130,61 @@
     [self setBackgroundOn:_on animated:animated];
     [self showFillLayer:!_on animated:animated];
 }
+
+- (void) setBackgroundOn:(BOOL)on animated:(BOOL)animated {
+    BOOL isOn = [[_backLayer valueForKey:@"isOn"] boolValue];
+    if (on != isOn) {
+        [_backLayer setValue:[NSNumber numberWithBool:on] forKey:@"isOn"];
+        if (animated) {
+            CABasicAnimation *animateColor = [CABasicAnimation animationWithKeyPath:@"fillColor"];
+            animateColor.duration = 0.22;
+            animateColor.fromValue = on ? (id)_tintColor.CGColor : (id)_onTintColor.CGColor;
+            animateColor.toValue = on ? (id)_onTintColor.CGColor : (id)_tintColor.CGColor;
+            animateColor.removedOnCompletion = NO;
+            animateColor.fillMode = kCAFillModeForwards;
+            [_backLayer addAnimation:animateColor forKey:@"animateColor"];
+            [CATransaction commit];
+        }else {
+            [_backLayer removeAllAnimations];
+            _backLayer.fillColor = on ? _onTintColor.CGColor : _tintColor.CGColor;
+        }
+    }
+}
+
+- (void) showFillLayer:(BOOL)show animated:(BOOL)animated {
+    BOOL isVisible = [[_fillLayer valueForKey:@"isVisible"] boolValue];
+    if (isVisible != show) {
+        [_fillLayer setValue:[NSNumber numberWithBool:show] forKey:@"isVisible"];
+        CGFloat scale = show ? 1.0 : 0.0;
+        if (animated) {
+            CGFloat from = show ? 0.0 : 1.0;
+            CABasicAnimation *animateScale = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+            animateScale.duration = 0.22;
+            animateScale.fromValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(from, from, 1.0)];
+            animateScale.toValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(scale, scale, 1.0)];
+            animateScale.removedOnCompletion = NO;
+            animateScale.fillMode = kCAFillModeForwards;
+            animateScale.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+            [_fillLayer addAnimation:animateScale forKey:@"animateScale"];
+        }else {
+            [_fillLayer removeAllAnimations];
+            _fillLayer.transform = CATransform3DMakeScale(scale,scale,1.0);
+        }
+    }
+}
+
+- (void) setPressed:(BOOL)pressed {
+    if (_pressed != pressed) {
+        _pressed = pressed;
+        
+        if (!_on) {
+            [self showFillLayer:!_pressed animated:YES];
+        }
+    }
+}
+
+#pragma mark -
+#pragma mark Appearance
 
 - (void) setTintColor:(UIColor *)tintColor {
     _tintColor = [tintColor retain];
@@ -300,12 +216,106 @@
     return [UIColor colorWithCGColor:_thumbLayer.fillColor];
 }
 
+#pragma mark -
+#pragma mark Interaction
+
+- (void)tapped:(UITapGestureRecognizer *)gesture
+{
+	if (gesture.state == UIGestureRecognizerStateEnded)
+		[self setOn:!self.on animated:YES];
+}
+
+- (void)toggleDragged:(UIPanGestureRecognizer *)gesture
+{
+	CGFloat minToggleX = 1.0;
+	CGFloat maxToggleX = self.bounds.size.width-self.bounds.size.height+1.0;
+    
+	if (gesture.state == UIGestureRecognizerStateBegan)
+	{
+		self.pressed = YES;
+        _dragging = YES;
+	}
+	else if (gesture.state == UIGestureRecognizerStateChanged)
+	{
+		CGPoint translation = [gesture translationInView:self];
+        
+		[CATransaction setDisableActions:YES];
+        
+		self.pressed = YES;
+        
+		CGFloat newX = _thumbLayer.frame.origin.x + translation.x;
+		if (newX < minToggleX) newX = minToggleX;
+		if (newX > maxToggleX) newX = maxToggleX;
+		_thumbLayer.frame = CGRectMake(newX,
+                                       _thumbLayer.frame.origin.y,
+                                       _thumbLayer.frame.size.width,
+                                       _thumbLayer.frame.size.height);
+        
+        if (CGRectGetMidX(_thumbLayer.frame) > CGRectGetMidX(self.bounds)
+            && ![[_backLayer valueForKey:@"isOn"] boolValue]) {
+            [self setBackgroundOn:YES animated:YES];
+        }else if (CGRectGetMidX(_thumbLayer.frame) < CGRectGetMidX(self.bounds)
+                  && [[_backLayer valueForKey:@"isOn"] boolValue]){
+            [self setBackgroundOn:NO animated:YES];
+        }
+        
+        
+		[gesture setTranslation:CGPointZero inView:self];
+	}
+	else if (gesture.state == UIGestureRecognizerStateEnded)
+	{
+		CGFloat toggleCenter = CGRectGetMidX(_thumbLayer.frame);
+        [self setOn:(toggleCenter > CGRectGetMidX(self.bounds)) animated:YES];
+        _dragging = NO;
+        self.pressed = NO;
+	}
+    
+	CGPoint locationOfTouch = [gesture locationInView:self];
+	if (CGRectContainsPoint(self.bounds, locationOfTouch))
+		[self sendActionsForControlEvents:UIControlEventTouchDragInside];
+	else
+		[self sendActionsForControlEvents:UIControlEventTouchDragOutside];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	[super touchesBegan:touches withEvent:event];
+    
+    self.pressed = YES;
+	
+	[self sendActionsForControlEvents:UIControlEventTouchDown];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	[super touchesEnded:touches withEvent:event];
+    if (!_dragging) {
+        self.pressed = NO;
+    }
+	[self sendActionsForControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	[super touchesCancelled:touches withEvent:event];
+    if (!_dragging) {
+        self.pressed = NO;
+    }
+	[self sendActionsForControlEvents:UIControlEventTouchUpOutside];
+}
+
+#pragma mark -
+#pragma mark Thumb Frame
+
 - (CGRect) thumbFrameForState:(BOOL)isOn {
     return CGRectMake(isOn ? self.bounds.size.width-self.bounds.size.height+1.0 : 1.0,
-                                         1.0,
-                                         self.bounds.size.height-2.0,
-                                         self.bounds.size.height-2.0);
+                      1.0,
+                      self.bounds.size.height-2.0,
+                      self.bounds.size.height-2.0);
 }
+
+#pragma mark -
+#pragma mark Dealloc
 
 - (void) dealloc {
     [_tintColor release], _tintColor = nil;
