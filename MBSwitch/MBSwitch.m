2ebc7a18e8748baf2b2e94be61f5ad1ec4e9f251
Dave Weston <dweston@yammer-inc.com>
2013-09-16 22:59:41 -0700

    Only disable local animations when animated == NO

--- MBSwitch/MBSwitch.m
+++ MBSwitch/MBSwitch.m
@@ -119,16 +119,18 @@
         _on = on;
         [self sendActionsForControlEvents:UIControlEventValueChanged];
     }
+
+    [CATransaction begin];
     if (animated) {
-        [CATransaction begin];
         [CATransaction setAnimationDuration:0.3];
         [CATransaction setDisableActions:NO];
         _thumbLayer.frame = [self thumbFrameForState:_on];
-        [CATransaction commit];
     }else {
         [CATransaction setDisableActions:YES];
         _thumbLayer.frame = [self thumbFrameForState:_on];
     }
+    [CATransaction commit];
+
     [self setBackgroundOn:_on animated:animated];
     [self showFillLayer:!_on animated:animated];
 }
